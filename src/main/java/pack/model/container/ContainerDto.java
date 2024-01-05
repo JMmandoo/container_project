18ddfb9739e3c8bc204663ee70ba56d806e603ea
Jae min lee <139545432+JMmandoo@users.noreply.github.com>
2024-01-05 13:23:45 +0900

    Update ContainerDto.java

--- src/main/java/pack/model/container/ContainerDto.java
+++ src/main/java/pack/model/container/ContainerDto.java
@@ -4,10 +4,9 @@ import lombok.Data;
 
 @Data
 public class ContainerDto {
-	// 민혁
 	private int  cont_price;
 	private String cont_addr, cont_size, cont_image, owner_name, cont_status, business_num;
 	private String  cont_name,  owner_phone, owner_num, cont_area, cont_explain, cont_no;
-	private double cont_we, cont_kyung; //재민 추가
+	private double cont_we, cont_kyung;
 
 }
