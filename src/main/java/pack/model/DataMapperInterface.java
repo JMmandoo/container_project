f66ecc73de8b91bb155f445f8b288bcc29151176
JMmandoo <leejeamon1994@gmail.com>
2024-01-08 23:43:03 +0900

    style: DataMapperInter 인터페이스에서 불필요한 주석 제거

--- src/main/java/pack/model/DataMapperInterface.java
+++ src/main/java/pack/model/DataMapperInterface.java
@@ -25,43 +25,47 @@ public interface DataMapperInterface {
 
    @Select("select count(*) from user ")
    int totalUser();
-   
+
    @Delete("delete from user where user_id=#{user_id}")
    int userdeleteData(String user_id);
-   
-   @Select("SELECT COUNT(*) FROM user")  // 사용자 테이블에 대한 SQL 쿼리
+
+   @Select("SELECT COUNT(*) FROM user")
+      // 사용자 테이블에 대한 SQL 쿼리
    int usercount();
-   
+
    // owner sql문들-------------------------------------------------
    @Select("select * from owner")
    List<OwnerDto> selectAll2();
-   
+
    @Select("SELECT * FROM owner WHERE business_num LIKE CONCAT('%', #{searchValue}, '%')")
    List<OwnerDto> selectSearch2(FormBean bean);
 
    @Select("select count(*) from owner")
    int ownerrecords();
-   
+
    @Delete("delete from owner where business_num=#{business_num}")
    int ownerdeleteData(String business_num);
-   
+
    // Container sql문들-------------------------------------------------
    @Select("select * from container")
    List<ContainerDto> selectAll3();
-   
+
    @Select("select count(*) from container")
    int totalContainer();
-   
+
    @Select("SELECT * FROM container WHERE cont_name LIKE CONCAT('%', #{searchValue}, '%')")
    List<ContainerDto> selectSearch3(FormBean bean);
-   
-   @Select("select * from container where cont_no=#{cont_no}")  // 세부정보 보기
+
+   @Select("select * from container where cont_no=#{cont_no}")
+      // 세부정보 보기
    ContainerDto selectOne(String cont_no);
-   
-   
-   @Delete("delete from container where cont_no=#{cont_no}")   // 삭제하기
+
+
+   @Delete("delete from container where cont_no=#{cont_no}")
+      // 삭제하기
    int delete(String cont_no);
 
    // 마이페이지에서 자신이 사용한 리뷰/평가 누르면 창고의 번호, 주소, 사진 출력-------------------
    @Select("select * from container")
    List<ContainerDto> selectAll4();
+}
